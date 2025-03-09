cd61de1223cc72031247425b6a74d754111b120c
anujgupta <anuj.gupta@telekom-digital.com>
2025-03-09 20:04:53 +0530

    Uncommented the code

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -1,24 +1,24 @@
-// const {test, expect} = require ('@playwright/test');
+const {test, expect} = require ('@playwright/test');
 
 
-// test("My First Test", async function({page})
-// {
-// expect(12).toBe(12);
-// })
+test("My First Test", async function({page})
+{
+expect(12).toBe(12);
+})
 
-// test("My second Test", async function({page}){
-//     expect(12).toBe(12);
-// })
+test("My second Test", async function({page}){
+    expect(12).toBe(12);
+})
 
-// test.skip("My third test case", async function({page}){
-//     expect("Anuj").toContain("AnujG");
-// })
+test.skip("My third test case", async function({page}){
+    expect("Anuj").toContain("AnujG");
+})
 
-// test("My Fourth Test", async function({page}){
-//     expect(true).toBeTruthy();
-//     expect(false).toBeFalsy();
-// })
+test("My Fourth Test", async function({page}){
+    expect(true).toBeTruthy();
+    expect(false).toBeFalsy();
+})
 
-// test("My Fifth Test", async function({page}){
-//     expect("Automation QA".includes("QA")).toBeTruthy();
-// })
+test("My Fifth Test", async function({page}){
+    expect("Automation QA".includes("QA")).toBeTruthy();
+})
